1773733c97387c6d9fe3b467db67618d1add4877
James Ayvaz <james.ayvaz@gmail.com>
2016-01-25 00:08:52 -0600

    moved methods into layer.js
    fixed text layer positioning

--- src/export/element/layer.js
+++ src/export/element/layer.js
@@ -76,3 +76,168 @@ function getLayerPosition(layer, containerWidth, containerHeight) {
     };
 }
 
+function parsePositionProperty(layer) {
+    var comp = layer.containingComp;
+    var layerTransform = layer.property("ADBE Transform Group");
+    var prop = layerTransform.property("ADBE Position");
+    return parseMarkerProperty(comp, prop, "Position", "position", function (index, keyTime, keyValue) {
+        if (index == 1) {
+            return [
+                keyTime,
+                {"x": rel(keyValue[0], comp.width), "y": rel(keyValue[1], comp.height)}
+            ];
+        }
+        else {
+            return [
+                keyTime,
+                {"x": rel(keyValue[0], comp.width), "y": rel(keyValue[1], comp.height)},
+                {
+                    "name": "linear",
+                    "x1": 1,
+                    "y1": 1,
+                    "x2": 1,
+                    "y2": 1
+                }
+            ];
+        }
+    });
+}
+
+function parseMarkerProperty(comp, prop, id, propname, callback) {
+    if (prop) {
+        var duration = 0;
+        var keyframes = [];
+        for (var i = 0; i < prop.numKeys; i++) {
+            var keyTime = prop.keyTime(i + 1);
+            duration += keyTime;
+        }
+
+        for (var i = 0; i < prop.numKeys; i++) {
+            var keyTime = prop.keyTime(i + 1) / duration;
+            var keyValue = prop.keyValue(i + 1);
+
+            var frame = callback(i, keyTime, keyValue);
+            if (frame) {
+                keyframes.push(frame);
+            }
+        }
+        if (keyframes.length > 0) {
+            return {
+                "type": "animation",
+                "id": id,
+                "duration": duration,
+                "delay": comp.delay ? comp.delay : 0,
+                "property": propname,
+                "keyframes": keyframes
+            };
+        }
+    }
+}
+
+function parseScaleProperty(layer) {
+    var comp = layer.containingComp;
+    var layerTransform = layer.property("ADBE Transform Group");
+    var prop = layerTransform.property("ADBE Scale");
+    return parseMarkerProperty(comp, prop, "Scale", "scale", function (index, keyTime, keyValue) {
+        if (index == 1) {
+            return [
+                keyTime,
+                {"sx": (keyValue[0] / 100), "sy": (keyValue[1] / 100)}
+            ];
+        }
+        else {
+            return [
+                keyTime,
+                {"sx": (keyValue[0] / 100), "sy": (keyValue[1] / 100)},
+                {
+                    "name": "linear",
+                    "x1": 1,
+                    "y1": 1,
+                    "x2": 1,
+                    "y2": 1
+                }
+            ];
+        }
+    });
+}
+
+function parseRotationProperty(layer) {
+    var comp = layer.containingComp;
+    var layerTransform = layer.property("ADBE Transform Group");
+    var prop = layerTransform.property("ADBE Rotate Z");
+    return parseMarkerProperty(comp, prop, "Rotation", "rotation", function (index, keyTime, keyValue) {
+        if (index == 1) {
+            return [
+                keyTime,
+                keyValue
+            ];
+        }
+        else {
+            return [
+                keyTime,
+                keyValue,
+                {
+                    "name": "linear",
+                    "x1": 1,
+                    "y1": 1,
+                    "x2": 1,
+                    "y2": 1
+                }
+            ];
+        }
+    });
+}
+
+function parseOpacityProperty(layer) {
+    var comp = layer.containingComp;
+    var layerTransform = layer.property("ADBE Transform Group");
+    var prop = layerTransform.property("ADBE Opacity");
+    return parseMarkerProperty(comp, prop, "Opacity", "opacity", function (index, keyTime, keyValue) {
+        if (index == 1) {
+            return [
+                keyTime,
+                keyValue
+            ];
+        }
+        else {
+            return [
+                keyTime,
+                (keyValue / 100),
+                {
+                    "name": "linear",
+                    "x1": 1,
+                    "y1": 1,
+                    "x2": 1,
+                    "y2": 1
+                }
+            ];
+        }
+    });
+}
+
+
+function getLayerAnimations(layer) {
+    var animations = [];
+
+    var scaleAnim = parseScaleProperty(layer);
+    if (scaleAnim) {
+        animations.push(scaleAnim);
+    }
+
+    var posAnim = parsePositionProperty(layer);
+    if (posAnim) {
+        animations.push(posAnim);
+    }
+
+    var rotAnim = parseRotationProperty(layer);
+    if (rotAnim) {
+        animations.push(rotAnim);
+    }
+
+    var opacityAnim = parseOpacityProperty(layer);
+    if (opacityAnim) {
+        animations.push(opacityAnim);
+    }
+
+    return animations;
+}
