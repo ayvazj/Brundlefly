5f02356b485ef11bc4feb630158595b3690eb27f
James Ayvaz <james.ayvaz@gmail.com>
2016-01-25 00:45:03 -0600

    fixed line endings

--- src/export/transform/anchor.js
+++ src/export/transform/anchor.js
@@ -1,1 +1,21 @@
-﻿function getAnchor(data, transform) {    var obj;    if (data.property('ADBE Anchor Point') instanceof Property) {        obj = data.property('ADBE Anchor Point');    } else if (data.property('ADBE Vector Anchor') instanceof Property) {        obj = data.property('ADBE Vector Anchor');    } else {        return null;    }        if (obj.isTimeVarying || obj.value[0] !== 0 || obj.value[1] !== 0) {        var anchor = getProperty(obj);        anchor = removeZValue(anchor);        anchor = roundValue(anchor);        anchor = normalizeKeyframes(anchor);        transform.anchor = anchor;    }}
+﻿function getAnchor(data, transform) {
+    var obj;
+
+    if (data.property('ADBE Anchor Point') instanceof Property) {
+        obj = data.property('ADBE Anchor Point');
+    } else if (data.property('ADBE Vector Anchor') instanceof Property) {
+        obj = data.property('ADBE Vector Anchor');
+    } else {
+        return null;
+    }
+    
+    if (obj.isTimeVarying || obj.value[0] !== 0 || obj.value[1] !== 0) {
+
+        var anchor = getProperty(obj);
+        anchor = removeZValue(anchor);
+        anchor = roundValue(anchor);
+        anchor = normalizeKeyframes(anchor);
+
+        transform.anchor = anchor;
+    }
+}
--- src/export/utils/logger.js
+++ src/export/utils/logger.js
@@ -1,1 +1,49 @@
-﻿function LogLevel() {}LogLevel.DEBUG = 1;LogLevel.INFO = 2;LogLevel.WARN = 3;LogLevel.ERROR = 4;LogLevel.FATAL = 5;function Logger(level) {    this.level = level || 0;    this.mute = false;}Logger.prototype.inspect = function (object) {    try {        for (var prop in object) {            this.puts(prop);        }    }    catch (err) {        this.puts(err);    }};Logger.prototype.log = function (message) {    if (!this.mute) {        $.writeln(message);    }};Logger.prototype.debug = function (message) {    if (this.level) {        $.writeln(message);    }};Logger.prototype.printprops = function (parent) {    this.log("\n\n>>>>>>>>>>");    for (var i = 1; i <= parent.numProperties; ++i) {        var prop = parent.property(i);        this.log(prop.matchName);    }    this.log("<<<<<<<<<<\n\n");};if (!console) {    var console = new Logger(LogLevel.DEBUG);}
+﻿function LogLevel() {
+}
+LogLevel.DEBUG = 1;
+LogLevel.INFO = 2;
+LogLevel.WARN = 3;
+LogLevel.ERROR = 4;
+LogLevel.FATAL = 5;
+
+function Logger(level) {
+    this.level = level || 0;
+    this.mute = false;
+}
+
+Logger.prototype.inspect = function (object) {
+    try {
+        for (var prop in object) {
+            this.puts(prop);
+        }
+    }
+    catch (err) {
+        this.puts(err);
+    }
+};
+
+Logger.prototype.log = function (message) {
+    if (!this.mute) {
+        $.writeln(message);
+    }
+};
+
+Logger.prototype.debug = function (message) {
+    if (this.level) {
+        $.writeln(message);
+    }
+};
+
+Logger.prototype.printprops = function (parent) {
+    this.log("\n\n>>>>>>>>>>");
+    for (var i = 1; i <= parent.numProperties; ++i) {
+        var prop = parent.property(i);
+        this.log(prop.matchName);
+    }
+    this.log("<<<<<<<<<<\n\n");
+};
+
+
+if (!console) {
+    var console = new Logger(LogLevel.DEBUG);
+}
